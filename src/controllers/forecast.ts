4e3c35ccbb39590651219828a035591cf1378497
Ronaldo Rocha <ronaldocuca@gmail.com>
2020-11-10 09:35:23 -0300

    ADD-prettier

--- src/controllers/forecast.ts
+++ src/controllers/forecast.ts
@@ -3,9 +3,8 @@ import { Request, Response } from 'express';
 
 @Controller('forecast')
 export class ForecastController {
-
-    @Get('')
-    public getForecastForLoggedUser(_: Request, res: Response): void {
-        res.send([{}])
-    }
+  @Get('')
+  public getForecastForLoggedUser(_: Request, res: Response): void {
+    res.send([{}]);
+  }
 }
