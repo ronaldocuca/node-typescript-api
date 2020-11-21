2fa5fa92030f3118e300883c38a039de1d421cd8
Ronaldo Rocha <ronaldocuca@gmail.com>
2020-11-21 17:26:20 -0300

    FIXED-c

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
 import { SetupServer } from './server';
 import config from 'config';
 
-const port =  process.env.PORT;
+const port : number =  (process.env.PORT || 3000) as number;
 const env = process.env.NODE_ENV;
 
 (async (): Promise<void> => {
-    const server = new SetupServer(config.get('App.port'));
+    let defaultPort = 3000;
+
+    if (env === 'production') {
+        defaultPort = port;
+    } else {
+        defaultPort = config.get('App.port');
+    }
+    const server = new SetupServer(defaultPort);
     await server.init();
     server.start();
-
-    console.log('port: ', port)
-    console.log('NODE_ENV: ', env)
-
 })();
