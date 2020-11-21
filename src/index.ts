4075a9899dc6fc1217a46e9ed198c02b6752e66c
Ronaldo Rocha <ronaldocuca@gmail.com>
2020-11-21 17:17:20 -0300

    FIXED-port test2

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import { SetupServer } from './server';
 import config from 'config';
+
 const port =  process.env.PORT;
+const env = process.env.NODE_ENV;
+
 (async (): Promise<void> => {
     const server = new SetupServer(config.get('App.port'));
     await server.init();
     server.start();
 
     console.log('port: ', port)
+    console.log('NODE_ENV: ', env)
+
 })();
